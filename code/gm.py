d92cea9b3701446964bb49f8af977b3c1264ef2f
Yi-Xiang Hu <1512687306@qq.com>
2022-10-22 21:33:01 +0800

    update

--- code/gm.py
+++ code/gm.py
@@ -1,32 +1,43 @@
-from cProfile import label
 from sklearn import datasets
 from sklearn.mixture import GaussianMixture
 import matplotlib.pyplot as plt
-# iris = datasets.load_iris()
-iris = datasets.load_iris()
-# print(iris)
+# load data
 X, y = datasets.load_iris(return_X_y=True)
 
-gm=GaussianMixture(n_components=3,covariance_type='full',random_state=0).fit(X,y)
-print(gm.means_)
-print(gm.covariances_)
-print(gm.weights_)
+# Gaussian Mixture Model
+# set covariance_type as spherical, diagonal, tied or full
+gm = GaussianMixture(n_components=3, covariance_type='full',
+                     random_state=0).fit(X, y)
 
-labels=gm.predict(X)
-# X=pd.concat
-fig=plt.figure()
-ax=fig.add_subplot(111,projection='3d')
+# print mean, variance and weight
+print("mean : ", gm.means_)
+print("variance : ", gm.covariances_)
+print("weight : ", gm.weights_)
+
+# predict
+labels = gm.predict(X)
+
+# plot
+fig = plt.figure(figsize=(12, 6))
+ax = fig.add_subplot(121, projection='3d')
+for i in range(len(y)):
+    if y[i] == 0:
+        ax.scatter(X[i][0], X[i][1], X[i][2], c='r')
+    elif y[i] == 1:
+        ax.scatter(X[i][0], X[i][1], X[i][2], c='blue')
+    elif y[i] == 2:
+        ax.scatter(X[i][0], X[i][1], X[i][2], c='g')
+ax.set_title("truth")
+
+ax = fig.add_subplot(122, projection='3d')
 for i in range(len(labels)):
-    if labels[i]==0:
-        ax.scatter(X[i][0],X[i][1],X[i][2],c='r')
-    elif labels[i]==1:
-        ax.scatter(X[i][0],X[i][1],X[i][2],c='blue')
-    elif labels[i]==2:
-        ax.scatter(X[i][0],X[i][1],X[i][2],c='g')
-
+    if labels[i] == 0:
+        ax.scatter(X[i][0], X[i][1], X[i][2], c='yellow')
+    elif labels[i] == 1:
+        ax.scatter(X[i][0], X[i][1], X[i][2], c='grey')
+    elif labels[i] == 2:
+        ax.scatter(X[i][0], X[i][1], X[i][2], c='brown')
+ax.set_title("predict")
+plt.savefig('D:\\AMCS_2022\\result\\result.png')
 plt.show()
 
-print(labels)
-print("***")
-print(X)
-# plt.scatter()
--- code/naive_bayes.py
+++ code/naive_bayes.py
@@ -1,31 +1,51 @@
 
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import BernoulliNB, CategoricalNB, ComplementNB
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
 from sklearn.preprocessing import OrdinalEncoder
+from sklearn.metrics import f1_score, recall_score, precision_score
 import pandas as pd
-test_file='D:\\AMCS_2022\\data\\Bayesian_Dataset_test.csv'
-OrdEnc=OrdinalEncoder()
-df=pd.read_csv(test_file,header=None)
+# load data
+train_file = 'D:\\AMCS_2022\\data\\Bayesian_Dataset_train.csv'  # training set
+df = pd.read_csv(train_file, header=None)
+X_train = df.iloc[:, :-1]
+y_train = df.iloc[:, -1]
 
-X_test=df.iloc[:,:-1]
-# print(len(X_test))
-y_test=df.iloc[:,-1]
-train_file='D:\\AMCS_2022\\data\\Bayesian_Dataset_train.csv'
-df=pd.read_csv(train_file,header=None)
-X_train=df.iloc[:,:-1]
-y_train=df.iloc[:,-1]
-X=pd.concat([X_test,X_train])
+test_file = 'D:\\AMCS_2022\\data\\Bayesian_Dataset_test.csv'  # test set
+df = pd.read_csv(test_file, header=None)
+X_test = df.iloc[:, :-1]
+X_test_before_transform = X_test
+y_test = df.iloc[:, -1]
 
-X=OrdEnc.fit_transform(X)
-X=pd.DataFrame(X)
-X_test=X.iloc[:len(X_test),:]
-# print(X_test)
-X_train=X.iloc[len(X_test):,:]
-# print(X_train)
-# print(y_test)
-# print(y_train)
-# X, y = load_iris(return_X_y=True)
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
-gnb = GaussianNB()
-y_pred = gnb.fit(X_train, y_train).predict(X_test)
+X = pd.concat([X_test, X_train])
+
+# data preprocessing
+
+OrdEnc = OrdinalEncoder()
+X = OrdEnc.fit_transform(X)
+X = pd.DataFrame(X)
+X_test = X.iloc[:len(X_test), :]
+X_train = X.iloc[len(X_test):, :]
+
+# naive bayes
+# select Naive Bayes classifier
+# and set parameters
+# nb = BernoulliNB()  # Naive Bayes classifier for multivariate Bernoulli models.
+nb = GaussianNB(var_smoothing=1e-11)  # Gaussian Naive Bayes
+# nb = CategoricalNB(alpha=1.0,fit_prior=True)  # Naive Bayes classifier for categorical features.
+# nb = ComplementNB()  # Complement Naive Bayes classifier
+# nb = MultinomialNB()  # Naive Bayes classifier for multinomial models
+y_pred = nb.fit(X_train, y_train).predict(X_test)
+
+# print result statistics
+print("positive label : ' >50K'")
 print("Number of mislabeled points out of a total %d points : %d"
       % (X_test.shape[0], (y_test != y_pred).sum()))
+print("Accuracy = %.4f" % (1-(y_test != y_pred).sum()/X_test.shape[0]))
+print("Precision = %.4f" % (precision_score(y_test, y_pred, pos_label=' >50K')))
+print("Recall = %.4f" % (recall_score(y_test, y_pred, pos_label=' >50K')))
+print("F1_score = %.4f" % (f1_score(y_test, y_pred, pos_label=' >50K')))
+
+# save prediction
+y_pred = pd.Series(y_pred)
+result = pd.concat([X_test_before_transform, y_test, y_pred], axis=1)
+result.to_csv("D:\\AMCS_2022\\data\\Bayesian_Dataset_pred.csv", header=0)
